4e70ce1ef86e19bd6d9692e0342d8a2187e1fb10
robert <robert@192.168.0.107>
2012-01-05 21:31:15 +0100

    Applied standard formatting

--- udf_infusion.c
+++ udf_infusion.c
@@ -530,7 +530,7 @@ char *cut(UDF_INIT *initid, UDF_ARGS *args,
 				goto done;
 			}
 		}
-done:
+	done:
 		if (-1 == i) {
 			*length = sl < max ? sl : max;
 		} else {
@@ -676,156 +676,158 @@ static char *_translate_string(UDF_ARGS *args, char *result, unsigned long *leng
 		unsigned short s = *((unsigned short *) ptr);
 
 		if (BTW(c, 'a', 'z') || BTW(c, '0', '9')) {
-			*(result++) = c;		add = 1;
+			*(result++) = c;
+			add = 1;
 		} else if (BTW(c, 'A', 'Z')) {
-			*(result++) = c | 32;	add = 1;
+			*(result++) = c | 32;
+			add = 1;
 		} else switch (s) {
-		case EN(0x9fc3):
-			ADD2I("ss");
-			break;
-		case EN(0x84c3): case EN(0xa4c3):
-		case EN(0xa6c3): case EN(0x86c3):
-			ADD2I("ae");
-			break;
-		case EN(0x96c3): case EN(0xb6c3):
-			ADD2I("oe");
-			break;
-		case EN(0x9cc3): case EN(0xbcc3):
-			ADD2I("ue");
-			break;
-		case EN(0xa0c3): case EN(0xa1c3):
-		case EN(0xa2c3): case EN(0xa3c3):
-		case EN(0xa5c3): case EN(0x80c3):
-		case EN(0x81c3): case EN(0x82c3):
-		case EN(0x83c3): case EN(0x85c3):
-			ADD1I("a");
-			break;
-		case EN(0xa8c3): case EN(0xa9c3):
-		case EN(0xaac3): case EN(0xabc3):
-		case EN(0x88c3): case EN(0x89c3):
-		case EN(0x8ac3): case EN(0x8bc3):
-			ADD1I("e");
-			break;
-		case EN(0xacc3): case EN(0xadc3):
-		case EN(0xaec3): case EN(0xafc3):
-		case EN(0x8cc3): case EN(0x8dc3):
-		case EN(0x8ec3): case EN(0x8fc3):
-			ADD1I("i");
-			break;
-		case EN(0xb0c3): case EN(0xb2c3):
-		case EN(0xb3c3): case EN(0xb4c3):
-		case EN(0xb5c3): case EN(0x92c3):
-		case EN(0x93c3): case EN(0x94c3):
-		case EN(0x95c3):
-			ADD1I("o");
-			break;
-		case EN(0xb9c3): case EN(0xbac3):
-		case EN(0xbbc3): case EN(0x99c3):
-		case EN(0x9ac3): case EN(0x9bc3):
-		case EN(0xb5c2):
-			ADD1I("u");
-			break;
-		case EN(0x91c3): case EN(0xb1c3):
-			ADD1I("n");
-			break;
-		case EN(0x87c3): case EN(0xa7c2):
-		case EN(0xa2c2): case EN(0xa7c3):
-			ADD1I("c");
-			break;
-		case EN(0x9ec3): case EN(0xbec3):
-			ADD1I("p");
-			break;
-		case EN(0xbfc3): case EN(0xa5c2):
-		case EN(0x9dc3): case EN(0xbdc3):
-			ADD1I("y");
-			break;
-		case EN(0x97c3):
-			ADD1I("x");
-			break;
-		case EN(0xaec2):
-			ADD1I("r");
-			break;
-		case EN(0x90c3):
-			ADD1I("d");
-			break;
-		default:
-			switch (c) {
-			case 223:
-				ADD2("ss");
+			case EN(0x9fc3):
+				ADD2I("ss");
 				break;
-			case 196: case 198:
-			case 228: case 230:
-				ADD2("ae");
+			case EN(0x84c3): case EN(0xa4c3):
+			case EN(0xa6c3): case EN(0x86c3):
+				ADD2I("ae");
 				break;
-			case 214: case 246:
-				ADD2("oe");
+			case EN(0x96c3): case EN(0xb6c3):
+				ADD2I("oe");
 				break;
-			case 220: case 252:
-				ADD2("ue");
+			case EN(0x9cc3): case EN(0xbcc3):
+				ADD2I("ue");
 				break;
-			case 192: case 193:
-			case 194: case 195:
-			case 197: case 224:
-			case 225: case 226:
-			case 227: case 229:
-				ADD1("a");
+			case EN(0xa0c3): case EN(0xa1c3):
+			case EN(0xa2c3): case EN(0xa3c3):
+			case EN(0xa5c3): case EN(0x80c3):
+			case EN(0x81c3): case EN(0x82c3):
+			case EN(0x83c3): case EN(0x85c3):
+				ADD1I("a");
 				break;
-			case 200: case 201:
-			case 202: case 203:
-			case 232: case 233:
-			case 234: case 235:
-				ADD1("e");
+			case EN(0xa8c3): case EN(0xa9c3):
+			case EN(0xaac3): case EN(0xabc3):
+			case EN(0x88c3): case EN(0x89c3):
+			case EN(0x8ac3): case EN(0x8bc3):
+				ADD1I("e");
 				break;
-			case 161: case 204:
-			case 205: case 206:
-			case 207: case 236:
-			case 237: case 238:
-			case 239:
-				ADD1("i");
+			case EN(0xacc3): case EN(0xadc3):
+			case EN(0xaec3): case EN(0xafc3):
+			case EN(0x8cc3): case EN(0x8dc3):
+			case EN(0x8ec3): case EN(0x8fc3):
+				ADD1I("i");
 				break;
-			case 210: case 211:
-			case 212: case 213:
-			case 240: case 242:
-			case 243: case 244:
-			case 245:
-				ADD1("o");
+			case EN(0xb0c3): case EN(0xb2c3):
+			case EN(0xb3c3): case EN(0xb4c3):
+			case EN(0xb5c3): case EN(0x92c3):
+			case EN(0x93c3): case EN(0x94c3):
+			case EN(0x95c3):
+				ADD1I("o");
 				break;
-			case 181: case 217:
-			case 218: case 219:
-			case 249: case 250:
-			case 251:
-				ADD1("u");
+			case EN(0xb9c3): case EN(0xbac3):
+			case EN(0xbbc3): case EN(0x99c3):
+			case EN(0x9ac3): case EN(0x9bc3):
+			case EN(0xb5c2):
+				ADD1I("u");
 				break;
-			case 209: case 241:
-				ADD1("n");
+			case EN(0x91c3): case EN(0xb1c3):
+				ADD1I("n");
 				break;
-			case 162: case 169:
-			case 199: case 231:
-				ADD1("c");
+			case EN(0x87c3): case EN(0xa7c2):
+			case EN(0xa2c2): case EN(0xa7c3):
+				ADD1I("c");
 				break;
-			case 222: case 254:
-				ADD1("p");
+			case EN(0x9ec3): case EN(0xbec3):
+				ADD1I("p");
 				break;
-			case 165: case 221:
-			case 253: case 255:
-				ADD1("y");
+			case EN(0xbfc3): case EN(0xa5c2):
+			case EN(0x9dc3): case EN(0xbdc3):
+				ADD1I("y");
 				break;
-			case 215:
-				ADD1("x");
+			case EN(0x97c3):
+				ADD1I("x");
 				break;
-			case 174:
-				ADD1("r");
+			case EN(0xaec2):
+				ADD1I("r");
 				break;
-			case 208:
-				ADD1("d");
+			case EN(0x90c3):
+				ADD1I("d");
 				break;
 			default:
-				if (add) {
-					*(result++) = separator;
-					add = 0;
+				switch (c) {
+				case 223:
+					ADD2("ss");
+					break;
+				case 196: case 198:
+				case 228: case 230:
+					ADD2("ae");
+					break;
+				case 214: case 246:
+					ADD2("oe");
+					break;
+				case 220: case 252:
+					ADD2("ue");
+					break;
+				case 192: case 193:
+				case 194: case 195:
+				case 197: case 224:
+				case 225: case 226:
+				case 227: case 229:
+					ADD1("a");
+					break;
+				case 200: case 201:
+				case 202: case 203:
+				case 232: case 233:
+				case 234: case 235:
+					ADD1("e");
+					break;
+				case 161: case 204:
+				case 205: case 206:
+				case 207: case 236:
+				case 237: case 238:
+				case 239:
+					ADD1("i");
+					break;
+				case 210: case 211:
+				case 212: case 213:
+				case 240: case 242:
+				case 243: case 244:
+				case 245:
+					ADD1("o");
+					break;
+				case 181: case 217:
+				case 218: case 219:
+				case 249: case 250:
+				case 251:
+					ADD1("u");
+					break;
+				case 209: case 241:
+					ADD1("n");
+					break;
+				case 162: case 169:
+				case 199: case 231:
+					ADD1("c");
+					break;
+				case 222: case 254:
+					ADD1("p");
+					break;
+				case 165: case 221:
+				case 253: case 255:
+					ADD1("y");
+					break;
+				case 215:
+					ADD1("x");
+					break;
+				case 174:
+					ADD1("r");
+					break;
+				case 208:
+					ADD1("d");
+					break;
+				default:
+					if (add) {
+						*(result++) = separator;
+						add = 0;
+					}
 				}
 			}
-		}
 	}
 
 	if ((*length = result - start_res) > 0 && *(result - 1) == separator) {
@@ -1032,8 +1034,8 @@ longlong noverk(UDF_INIT *initid __attribute__((unused)), UDF_ARGS *args,
 
 	longlong i, res = 1;
 
-	longlong n = *((longlong *)args->args[0]);
-	longlong k = *((longlong *)args->args[1]);
+	longlong n = *((longlong *) args->args[0]);
+	longlong k = *((longlong *) args->args[1]);
 
 	if (!(k > 0 && n > 0 && n > k)) {
 		*is_null = 1;
@@ -1088,7 +1090,7 @@ void skewness_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char *error)
 
 	// http://people.xiph.org/~tterribe/notes/homs.html
 
-	double value = *((double *)args->args[0]);
+	double value = *((double *) args->args[0]);
 
 	data->count++;
 
@@ -1100,8 +1102,8 @@ void skewness_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char *error)
 	double shorten = delta * delta_n * (n - 1);
 
 	data->mean = data->mean + delta_n;
-	data->M3   = data->M3   + shorten * delta_n * (n - 2) - 3 * delta_n * data->M2;
-	data->M2   = data->M2   + shorten;
+	data->M3 = data->M3 + shorten * delta_n * (n - 2) - 3 * delta_n * data->M2;
+	data->M2 = data->M2 + shorten;
 }
 
 void skewness_deinit(UDF_INIT *initid)
@@ -1168,7 +1170,7 @@ void kurtosis_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char *error)
 
 	// http://people.xiph.org/~tterribe/notes/homs.html
 
-	double value = *((double *)args->args[0]);
+	double value = *((double *) args->args[0]);
 
 	data->count++;
 
@@ -1180,9 +1182,9 @@ void kurtosis_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char *error)
 	double shorten = delta * delta_n * (n - 1);
 
 	data->mean = data->mean + delta_n;
-	data->M4   = data->M4   + delta_n * (shorten * delta_n * (3 + n * (n - 3)) + 6 * delta_n * data->M2 - 4 * data->M3);
-	data->M3   = data->M3   + shorten * delta_n * (n - 2) - 3 * delta_n * data->M2;
-	data->M2   = data->M2   + shorten;
+	data->M4 = data->M4 + delta_n * (shorten * delta_n * (3 + n * (n - 3)) + 6 * delta_n * data->M2 - 4 * data->M3);
+	data->M3 = data->M3 + shorten * delta_n * (n - 2) - 3 * delta_n * data->M2;
+	data->M2 = data->M2 + shorten;
 }
 
 void kurtosis_deinit(UDF_INIT *initid)
@@ -1247,8 +1249,8 @@ void covariance_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char *error
 {
 	struct CovBuffer *data = (struct CovBuffer *) initid->ptr;
 
-	double x = *((double *)args->args[0]);
-	double y = *((double *)args->args[1]);
+	double x = *((double *) args->args[0]);
+	double y = *((double *) args->args[1]);
 
 	data->count++;
 
@@ -1274,7 +1276,6 @@ double covariance(UDF_INIT *initid __attribute__((unused)), UDF_ARGS *args,
 
 	return (data->c - data->x * data->y / data->count) / data->count;
 }
-
 
 inline static unsigned int doubleSlot(struct DoubleBuffer *buffer, double value, int m, int n)
 {
@@ -1317,7 +1318,7 @@ inline static void doublePush(struct DoubleBuffer *buffer, unsigned int step, do
 	function in this subclause, pointer arguments on such a call shall still
 	have valid values, as described in 7.1.4."
 	 */
-	memmove(&data[1 + step], &data[step], sizeof(*data) * (buffer->used - step));
+	memmove(&data[1 + step], &data[step], sizeof (*data) * (buffer->used - step));
 
 	data[step] = value;
 }
@@ -1397,7 +1398,6 @@ inline static void doublePush(struct DoubleBuffer *buffer, unsigned int step, do
 											\
 	} while (1)
 
-
 my_bool lesspart_init(UDF_INIT *initid, UDF_ARGS *args, char *message)
 {
 	struct DoubleBuffer* data = NULL;
@@ -1434,7 +1434,7 @@ void lesspart_deinit(UDF_INIT *initid)
 longlong lesspart(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error)
 {
 	struct DoubleBuffer *data = (struct DoubleBuffer *) initid->ptr;
-	double limit = *((double*)args->args[1]);
+	double limit = *((double*) args->args[1]);
 
 	double psum = 0;
 
@@ -1490,7 +1490,7 @@ void lesspartpct_deinit(UDF_INIT *initid)
 longlong lesspartpct(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error)
 {
 	struct DoubleBuffer *data = (struct DoubleBuffer *) initid->ptr;
-	double limit = *((double*)args->args[1]);
+	double limit = *((double*) args->args[1]);
 
 	double psum = 0;
 
@@ -1536,7 +1536,7 @@ void lessavg_clear(UDF_INIT* initid, char* is_null, char *error)
 void lessavg_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char *error)
 {
 	struct DoubleBuffer *data = (struct DoubleBuffer *) initid->ptr;
-	double value = *((double*)args->args[0]);
+	double value = *((double*) args->args[0]);
 
 	if (NULL == args->args[0])
 		return;
@@ -1561,7 +1561,7 @@ longlong lessavg(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error)
 
 	double /* volatile */ *ptr;
 	double limit = data->sum / data->used;
-	double *beg  = data->number;
+	double *beg = data->number;
 
 	if (1 == data->used % 2) {
 		count = (data->number[--data->used] < limit);
