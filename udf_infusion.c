914b09ed8a7e187d16a384c5213ae48d20b7b5d8
Robert <robert@xarg.org>
2012-07-14 00:20:40 +0200

    Added the famous TSQL row_number() function

--- udf_infusion.c
+++ udf_infusion.c
@@ -855,6 +855,49 @@ static char *_translate_string(UDF_ARGS *args, char *result, unsigned long *leng
 #undef ADD2
 #undef ADD2I
 
+my_bool row_number_init(UDF_INIT *initid, UDF_ARGS *args, char *message)
+{
+
+	longlong *data;
+
+	if (0 != args->arg_count) {
+		strcpy(message, "rownumber doesn't require an argument");
+		return 1;
+	}
+
+	if (NULL == (data = malloc(sizeof (*data)))) {
+		strcpy(message, "Couldn't allocate memory");
+		return 1;
+	}
+	*data = 0;
+
+	initid->ptr = (char *) data;
+	initid->const_item = 0;
+	initid->maybe_null = 1;
+
+	return 0;
+}
+
+longlong row_number(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error)
+{
+	longlong *data;
+
+	if (initid->ptr) {
+		data = (longlong *) initid->ptr;
+		return ++(*data);
+	}
+
+	*error = 1;
+	return 0;
+}
+
+void row_number_deinit(UDF_INIT *initid)
+{
+	if (initid->ptr) {
+		free(initid->ptr);
+	}
+}
+
 my_bool group_first_init(UDF_INIT *initid, UDF_ARGS *args, char *message)
 {
 	struct Buffer* data;
