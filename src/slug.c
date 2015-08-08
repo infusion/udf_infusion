d71845519852a4e14563d55a9aaacfbea8a14b2c
Robert Eisele <robert@xarg.org>
2015-08-08 20:10:32 +0200

    Added dynamic string allocation

--- src/slug.c
+++ src/slug.c
@@ -10,7 +10,8 @@ my_bool slug_init(UDF_INIT *initid, UDF_ARGS *args, char *message)
 
     args->arg_type[0] = STRING_RESULT;
 
-    initid->max_length = args->attribute_lengths[0] * 2;
+    initid->max_length = args->lengths[0] * 2;
+    initid->ptr = NULL;
     initid->const_item = 1;
     initid->maybe_null = 1;
 
@@ -21,10 +22,32 @@ char *slug(UDF_INIT *initid, UDF_ARGS *args,
         char *result, unsigned long *length,
         char *is_null, char *error __attribute__((unused)))
 {
+    char *ptr;
+
     if (NULL == args->args[0]) {
         *is_null = 1;
-        return 0;
+        return result;
     }
 
-    return _translate_string(args, result, length, '-');
+    if (initid->max_length <= 255) {
+        ptr = result;
+    } else if(NULL != initid->ptr) {
+        ptr = initid->ptr;
+    } else if (NULL == (ptr = malloc(initid->max_length))) {
+        *is_null = 1;
+        return result;
+    } else {
+        initid->ptr = ptr;
+    }
+    return _translate_string(args, ptr, length, '_');
 }
+
+my_bool slug_deinit(UDF_INIT *initid, UDF_ARGS *args, char *message) {
+    char *ptr = (char *) initid->ptr;
+
+    if (NULL != ptr) {
+        free(ptr);
+    }
+    return 0;
+}
+
